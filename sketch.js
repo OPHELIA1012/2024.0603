44c880f8a191854475372ebac7b937fb30286851
OPHELIA1012 <150638639+OPHELIA1012@users.noreply.github.com>
2024-06-03 10:12:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,6 @@
-/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
-
-MoveNet is developed by TensorFlow:
-https://www.tensorflow.org/hub/tutorials/movenet
-
-*/
-
-let video, bodypose, pose, keypoint, detector;
+let bodypose, pose, keypoint, detector;
 let poses = [];
+let img; // 新增一個變數來儲存圖片
 
 async function init() {
   const detectorConfig = {
@@ -18,14 +12,22 @@ async function init() {
   );
 }
 
-async function videoReady() {
-  console.log("video ready");
-  await getPoses();
+async function setup() {
+  createCanvas(640, 480);
+  img = loadImage('https://hackmd.io/_uploads/SybnLpYV0.gif', () => {
+    console.log('Image loaded');
+    image(img, 0, 0, width, height); // 繪製圖片到畫布上
+  });
+
+  await init();
+
+  stroke(255);
+  strokeWeight(5);
 }
 
 async function getPoses() {
   if (detector) {
-    poses = await detector.estimatePoses(video.elt, {
+    poses = await detector.estimatePoses(img.elt, {
       maxPoses: 2,
       //flipHorizontal: true,
     });
@@ -33,25 +35,8 @@ async function getPoses() {
   requestAnimationFrame(getPoses);
 }
 
-async function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO, videoReady);
-  video.size(width, height);
-  video.hide();
-  await init();
-
-  stroke(255);
-  strokeWeight(5);
-}
-
 function draw() {
-  image(video, 0, 0);
   drawSkeleton();
-  // flip horizontal
-  cam = get();
-  translate(cam.width, 0);
-  scale(-1, 1);
-  image(cam, 0, 0);
 }
 
 function drawSkeleton() {
@@ -104,23 +89,3 @@ function drawSkeleton() {
     }
   }
 }
-
-/* Points (view on left of screen = left part - when mirrored)
-  0 nose
-  1 left eye
-  2 right eye
-  3 left ear
-  4 right ear
-  5 left shoulder
-  6 right shoulder
-  7 left elbow
-  8 right elbow
-  9 left wrist
-  10 right wrist
-  11 left hip
-  12 right hip
-  13 left kneee
-  14 right knee
-  15 left foot
-  16 right foot
-*/
