84c788113063852212362671fc9dd282136b6401
OPHELIA1012 <150638639+OPHELIA1012@users.noreply.github.com>
2024-06-04 02:08:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,155 +1,78 @@
-function preload(){  //我的圖片檔
-  dogImg = loadImage("狗狗.gif")
-  }
-  
-  let video, bodypose, pose, keypoint, detector;
-  let poses = [];
-  
-  
-  
-  
-  async function init() {
-    const detectorConfig = {
-      modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
-    };
-    detector = await poseDetection.createDetector(
-      poseDetection.SupportedModels.MoveNet,
-      detectorConfig
-    );
-  }
-  
-  async function videoReady() {
-    console.log("video ready");
-    await getPoses();
-  }
-  
-  async function getPoses() {
-    if (detector) {
-      poses = await detector.estimatePoses(video.elt, {
-        maxPoses: 2,
-        //flipHorizontal: true,
-      });
-    }
-    requestAnimationFrame(getPoses);
-  }
-  
-  async function setup() {
-    createCanvas(640, 480);
-    video = createCapture(VIDEO, videoReady);
-    video.size(width, height);
-    video.hide();
-    await init();
-  
-    stroke(255);
-    strokeWeight(5);
-  }
-  
-  function draw() {
-    image(video, 0, 0);
-    drawSkeleton();
-    // flip horizontal
-    cam = get();
-    translate(cam.width, 0);
-    scale(-1, 1);
-    image(cam, 0, 0);
-   
-  }
-  
-  function drawSkeleton() {
+function drawSkeleton() {
     // Draw all the tracked landmark points
     for (let i = 0; i < poses.length; i++) {
       pose = poses[i];
-      // shoulder to wrist
-  
+      
+      // Draw text
       partA = pose.keypoints[0];
-  
       if(partA.score > 0.1){
-        push()
-          textSize(40) //大小
-          scale(-1,1)  //反向
-          text("412730169,游詠婕",partA.x-width,partA.y-150)  //我的名字
-        pop()
+        push();
+        textSize(40);
+        scale(-1,1);
+        text("412730169,游詠婕",partA.x-width,partA.y-150);
+        pop();
       }
-     
-      for (j = 5; j < 9; j++) {
-        if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
-          partA = pose.keypoints[j];
-          partB = pose.keypoints[j + 2];
-          line(partA.x, partA.y, partB.x, partB.y);
+      
+      // Draw lines and images
+      for (let j = 0; j < pose.keypoints.length; j++) {
+        keypoint = pose.keypoints[j];
+        
+        if (keypoint.score > 0.1) {
+          if (j === 1 || j === 3) { // 左眼和左耳
+            image(dogImg, keypoint.x, keypoint.y, 150, 150);
+          } else if (j === 2 || j === 4) { // 右眼和右耳
+            push();
+            scale(-1,1);
+            image(dogImg, width - keypoint.x, keypoint.y, 150, 150);
+            pop();
+          }
+          
+          // Draw lines (shoulder to wrist)
+          if (j >= 5 && j <= 8 && pose.keypoints[j + 2].score > 0.1) {
+            partA = keypoint;
+            partB = pose.keypoints[j + 2];
+            line(partA.x, partA.y, partB.x, partB.y);
+          }
         }
       }
-      // shoulder to shoulder
+      
+      // Draw shoulder to shoulder
       partA = pose.keypoints[7];
       partB = pose.keypoints[8];
       if (partA.score > 0.1 && partB.score > 0.1) {
-          //line(partA.x, partA.y, partB.x, partB.y);
-      push()
-        image(dogImg,partA.x, partA.y,150,150)  //左邊肩膀
-        image(dogImg,partB.x, partB.y,150,150)  //右邊肩膀
-      pop()
-       
-    }
-    // eyes
-    partA = pose.keypoints[1];
-    partB = pose.keypoints[2];
-    if (partA.score > 0.1 && partB.score > 0.1) {
-    push()
-          image(dogImg,partA.x, partA.y,150,150)  //左邊肩膀
-          image(dogImg,partB.x, partB.y,150,150)  //右邊肩膀
-    pop()
-         
-     
-    }
+        push();
+        image(dogImg, partA.x, partA.y, 150, 150); // 左边肩膀
+        image(dogImg, partB.x, partB.y, 150, 150); // 右边肩膀
+        pop();
       }
-      // hip to hip
+      
+      // Draw hip to hip
       partA = pose.keypoints[11];
       partB = pose.keypoints[12];
       if (partA.score > 0.1 && partB.score > 0.1) {
         line(partA.x, partA.y, partB.x, partB.y);
-       
       }
-      // shoulders to hips
+      
+      // Draw shoulders to hips
       partA = pose.keypoints[5];
       partB = pose.keypoints[11];
       if (partA.score > 0.1 && partB.score > 0.1) {
         line(partA.x, partA.y, partB.x, partB.y);
-       
       }
+      
       partA = pose.keypoints[6];
       partB = pose.keypoints[12];
       if (partA.score > 0.1 && partB.score > 0.1) {
         line(partA.x, partA.y, partB.x, partB.y);
-       
       }
-      // hip to foot
-      for (j = 11; j < 15; j++) {
+      
+      // Draw hip to foot
+      for (let j = 11; j < 15; j++) {
         if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
           partA = pose.keypoints[j];
           partB = pose.keypoints[j + 2];
           line(partA.x, partA.y, partB.x, partB.y);
-         
         }
       }
     }
-  
-
-  
-  /* Points (view on left of screen = left part - when mirrored)
-    0 nose
-    1 left eye
-    2 right eye
-    3 left ear
-    4 right ear
-    5 left shoulder
-    6 right shoulder
-    7 left elbow
-    8 right elbow
-    9 left wrist
-    10 right wrist
-    11 left hip
-    12 right hip
-    13 left kneee
-    14 right knee
-    15 left foot
-    16 right foot
-  */
+  }
